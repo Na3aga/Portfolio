85fb7f1b18a2443e6404b04c48b22b3d9c80a428
Nazar <100243666+na3aga-jsp@users.noreply.github.com>
2022-11-21 01:01:04 +0200

    added other information

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,30 +8,29 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Nazar Havryliuk </span>
+            from <span className="purple"> Kyiv, Ukraine.</span>
+            <br />I am a junior pursuing Master Degree in Applied Maths and Data Science in NTUU, Kyiv Polytechnic Institute.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from coding, I am interested in
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Learning about new Tech and fields
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> Improving my knowledge about cultures
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Travelling 
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Na3aga</footer>
         </blockquote>
       </Card.Body>
     </Card>
